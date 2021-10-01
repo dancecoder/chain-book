2f6671f5a039a2394e967424a70e7f294860f23b
sanchine <86965357+sanchine@users.noreply.github.com>
2021-10-01 10:25:07 +0500

    Add files via upload
    наполнители для src

---
+++ frontend/chain-book/src/App.js
@@ -0,0 +1,15 @@
+import logo_chain from './logo_chain.png';
+import './App.css';
+
+let App = () => (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo_chain} className="App-logo" alt="logo_chain" />
+        <p className="App-text">
+          CHAINBOX
+        </p>
+      </header>
+    </div>
+  );
+
+export default App;
