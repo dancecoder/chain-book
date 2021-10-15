6c2edd17c1c3eb40ae4f0bfbfb746ab8ff340212
eL3ctr074 <zodberg555@gmail.com>
2021-10-15 16:13:50 +0500

    Изменил все компоненты (сгенерировал заново))

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,43 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = req('mongoose');
-var Post = req('./post.js')
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const PORT = 3000;
-const DB_URL = 'mongodb+srv://admin:user@cluster0.dmqur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+var app = express();
 
-const app = express()
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.post("/", async (req, res) => {
-    try {
-        const {author, title, content, picture} = req.body
-        const post = await Post.create({author, title, content, picture})
-        console.log(req.body);
-        res.status(200).json('Server at working')
-    } catch (e){
-        res.status(500).json(e)
-    }
-    
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get("/", function(req, res){
-    console.log(req.query);
-    res.end("Hello, wonderful world!");
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-async function startApp(){
-    try{
-        await mongoose.connect(DB_URL)
-        app.listen(PORT, () => console.log('SERVER START, PORT:' + PORT))
-    } catch (e){
-        console.log(e)
-    }
-}
-
-startApp();
-
-app.listen(PORT,() => console.log('Server starting port:' + PORT))
+require('crypto').randomBytes(64).toString('hex');
+console.log(require('crypto').randomBytes(64).toString('hex'));
+module.exports = app;
