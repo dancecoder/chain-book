0bd875ada1d142afe57c795dc7616658b6a6d0db
sanchine <86965357+sanchine@users.noreply.github.com>
2021-10-26 18:25:42 +0500

    исправление семантики, добавление стиля

--- frontend/src/components/AvatarPage/AvatarPage.js
+++ frontend/src/components/AvatarPage/AvatarPage.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./AvatarPage.css";
 
 export const AvatarPage = () => {
     return (
         <div>
-            <Link className="home__Icon" to="/">НАЧАЛО</Link>
-            <p></p>
-
-            <h1>Имя Фамилия</h1>
+            <Link className="home__Icon" to="/">Начало</Link>
+            <h3>Имя Фамилия</h3>
             <img/>
-            <h3>Данные о пользователе</h3>
+            <h4>Данные о пользователе</h4>
         </div>
     )
 }
