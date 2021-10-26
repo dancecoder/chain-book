092119f91c65eb4898337a531a181d57264df007
sanchine <86965357+sanchine@users.noreply.github.com>
2021-10-26 18:28:42 +0500

    перенесены все функции из ненужной App.js

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,13 +1,20 @@
 import React from "react";
+import "./MainPage.css";
 import { Link } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
 
-
-export const MainPage = () => {
+export function MainPage({route}) {
+    MainPage.defaultProps = {
+        route: null
+    };
     return (
-        <div>
-            <h1 className="HomeText">Добро пожаловать в Chainbook!</h1>
-            <Link className="button__signup" to="/signup">РЕГИСТРАЦИЯ</Link>
-            <Link className="button__signup" to="/login">АВТОРИЗАЦИЯ</Link>
+        <div className="AppWork">
+            <div id="appframe" className="AppFrame">
+                <div>{renderRoutes(route.routes)}</div>
+                <h1 className="HomeText">Добро пожаловать в Chainbook!</h1>
+                <Link className="button__signup" to="/signup">Регистрация</Link>
+                <Link className="button__signup" to="/login">Вход</Link>
+            </div>
         </div>
     )
 }
